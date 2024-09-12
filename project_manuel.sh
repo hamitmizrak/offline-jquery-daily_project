19ddebbe3207cdff33785ff66dc7dc92718f7968
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-13 00:28:40 +0300

    success all

--- project_manuel.sh
+++ project_manuel.sh
@@ -6,37 +6,43 @@ chmod +x project_npm_local.sh
 chmod +x project_docker.sh
 chmod +x project_manuel.sh
 
-# Typescript compiler
-tsc 
-#nodemon --exec ts-node index.ts
-#node ./index.js
-
-# Eğer tsconfig.json içinde => "outDir": "./dist" yazarsam
-#node ./dist/index.js
-nodemon --exec ts-node ./dist/index.js
-
-
-
 # 1.YOL
-# node index.js
+# Typescript compiler
+#tsc 
+##node ./dist/index.js
+#nodemon --exec ts-node ./dist/index.js
+##nodemon --exec ts-node index.ts
+##node ./index.js
+## Eğer tsconfig.json içinde => "outDir": "./dist" yazarsam
+
 
 # 2.YOL
+npm run build
+npm run start
+
+
+
+
+# 3.YOL
+# node index.js
+
+# 4.YOL
 # & = anlamı
 # node index.js komutunun çalıştıktan sonra terminali "bloklayarak" (kilitleyerek) diğer komutlara geçişi engellemesidir.
 # Yani, node index.js çalıştığında terminal bu süreçte bekler ve docker-compose up -d komutunu çalıştırmaz.
 # node index.js &
 
-# 3.YOL
+# 5.YOL
 #npm start
 
-# 4.YOL
+# 6.YOL
 # Eğer biz bir script yazarsak run yazmak zorundayız.
 # npm run start
 
-# 5.YOL Nodemon ile ayağa kaldır
+# 7.YOL Nodemon ile ayağa kaldır
 # node monitoring
 # npm run nodemon
 
-# 6.YOL Nodemon ile ayağa kaldır
+# 8.YOL Nodemon ile ayağa kaldır
 # q: terminal çıktısındaki çok detayı gösterme
 #npm run nodemon_q
